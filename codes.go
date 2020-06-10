b2ed1f7e07efc9761cdc8301ef0368ba875a2c85
Alberts Zemzale <alberts.zemzale@gmail.com>
2020-06-10 18:11:23 +0300

    Fix 203

--- codes.go
+++ codes.go
@@ -75,16 +75,13 @@ The 202 response is intentionally noncommittal. Its purpose is to allow a server
 	{
 		Code: 203,
 		Name: "Non-authoritative Information",
-		Description: `
-
-The request was successful but the enclosed payload has been modified from that of the origin server's <a href="/200">200 OK</a> response by a transforming proxy<sup><a href="#ref-1">1</a></sup>.
+		Description: `The request was successful but the enclosed payload has been modified from that of the origin server's 200 OK response by a transforming proxy.
 
 This status code allows the proxy to notify recipients when a transformation has been applied, since that knowledge might impact later decisions regarding the content. For example, future cache validation requests for the content might only be applicable along the same request path (through the same proxies).
 
-The 203 response is similar to the Warning code of 214 Transformation Applied<sup><a href="#ref-2">2</a></sup>, which has the advantage of being applicable to responses with any status code.
+The 203 response is similar to the Warning code of 214 Transformation Applied, which has the advantage of being applicable to responses with any status code.
 
-A 203 response is cacheable by default; i.e., unless otherwise indicated by the method definition or explicit cache controls<sup><a href="#ref-3">3</a></sup>.
-`,
+A 203 response is cacheable by default; i.e., unless otherwise indicated by the method definition or explicit cache controls.`,
 	},
 	{
 		Code: 204,
