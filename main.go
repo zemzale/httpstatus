e687e3c96df0434fbf17465f53e35193ce3b1fba
Alberts Zemzale <alberts.zemzale@gmail.com>
2020-06-11 19:17:14 +0300

    Remove the debug option

--- main.go
+++ main.go
@@ -1,18 +1,12 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
 	"os"
 	"strconv"
 )
 
 func main() {
-	if os.Args[1] == "debug" {
-		debug()
-		os.Exit(0)
-	}
-
 	inputCode, err := strconv.Atoi(os.Args[1])
 	if err != nil {
 		fmt.Println("Invalid code")
@@ -35,34 +29,4 @@ func main() {
 
 	fmt.Println("No code with this number")
 	os.Exit(1)
-}
-
-func debug() {
-	f, err := os.Create("codeStatus.txt")
-	if err != nil {
-		panic(err)
-	}
-	defer f.Close()
-	for _, code := range HTTPCodes {
-		fmt.Printf("Code : %d\n", code.Code)
-		fmt.Printf("Name : %s\n", code.Name)
-		fmt.Printf("Description : %v\n", code.Description)
-	READAGAIN:
-		reader := bufio.NewReader(os.Stdin)
-		char, _, err := reader.ReadRune()
-		if err != nil {
-			panic(err)
-		}
-		switch char {
-		case 'y':
-			f.WriteString(fmt.Sprintf("%d : good\n", code.Code))
-		case 'n':
-			f.WriteString(fmt.Sprintf("%d : bad\n", code.Code))
-		default:
-			goto READAGAIN
-		}
-	}
-	if err := f.Sync(); err != nil {
-		panic(err)
-	}
 }
